c1fe98183ae777fb4c5687f8f57c83e52ff3db33
MonishaJ1997 <jkmonisha97@gmail.com>
2025-09-17 07:26:30 +0530

    Update manage.py

--- trainee_tracker/manage.py
+++ trainee_tracker/manage.py
@@ -5,7 +5,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trainee_tracker.backend.tracker_backend.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.tracker_backend.settings")
 
    
     try:
@@ -22,3 +22,4 @@ def main():
 if __name__ == '__main__':
 
     main()
+
